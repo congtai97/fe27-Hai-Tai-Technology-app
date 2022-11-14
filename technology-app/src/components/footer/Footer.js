efcb94ae20b909d07d125055b556332a7a2d79ba
taitcpd01633 <trancongtai997@gmail.com>
2022-11-14 15:24:14 +0700

    Update code

--- technology-app/src/components/footer/Footer.js
+++ technology-app/src/components/footer/Footer.js
@@ -1,11 +1,38 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import { FaFacebook, FaPhone } from "react-icons/fa";
+
 
 const date = new Date();
 const year = date.getFullYear();
 
 const Footer = () => {
-  return <div className={styles.footer}>&copy; {year} All Rights Reserved</div>;
+  return <div className={styles.footer}>
+    <div className={styles.rows}>
+      <div className={styles.left}>
+        <h4>Thông tin hỗ trợ</h4>
+        <div className={styles.email}>
+          <FaFacebook/>
+          <p>trancongtai997@gmail.com</p>
+        </div>
+        <div className={styles.email}>
+          <FaPhone/>
+          <p>+84 12345678</p>
+        </div>
+        
+      </div>
+      <div className={styles.center}>
+        <h4>Chăm Sóc Khách Hàng</h4>
+      </div>
+      <div className={styles.right}>
+        
+      </div>
+    </div>
+
+    <div className={styles.bottom}>
+          &copy; {year}
+    </div>
+  </div>;
 };
 
 export default Footer;
